75113f0820af2d2a4e2cbc3889d691e8926e94bf
Minjoon Lee <newdlakstod@gmail.com>
2026-08-25 07:06:02 +0900

    chore: bump version to 2.9.10 (114)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.g1.sketchbook"
         minSdk = 24
         targetSdk = 35
-        versionCode = 113
-        versionName = "2.9.9"
+        versionCode = 114
+        versionName = "2.9.10"
         vectorDrawables { useSupportLibrary = true }
     }
 
